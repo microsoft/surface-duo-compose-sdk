a5b68c5f083f116553cce51358171651a6b1bf2a
Kristen Halper <khalp@vt.edu>
2022-02-03 12:30:41 -0500

    Add more comments

--- ComposeTesting/library/src/main/java/com/microsoft/device/dualscreen/composetesting/DeviceModel.kt
+++ ComposeTesting/library/src/main/java/com/microsoft/device/dualscreen/composetesting/DeviceModel.kt
@@ -18,16 +18,32 @@ import androidx.test.uiautomator.UiDevice
  */
 
 /**
- * Coordinates taken from dual portrait point of view
- * Dimensions available here: https://docs.microsoft.com /dual-screen/android/surface-duo-dimensions
+ * Enum class that can be used to represent various device models and extract coordinates that can be used for
+ * simulating gestures in UI tests
+ *
+ * For Microsoft Surface Duo devices, the coordinates are all from the dual portrait point of view, and dimensions
+ * were taken from here: https://docs.microsoft.com /dual-screen/android/surface-duo-dimensions
+ *
+ * @param paneWidth: width of device panes in pixels in dual portrait mode (assumed to have panes of equal size)
+ * @param paneHeight: height of device panes in pixels in dual portrait mode (assumed to have panes of equal size)
+ * @param foldWidth: width of device fold in pixels in dual portrait mode
+ * @param leftX: x-coordinate of the center of the left pane in dual portrait mode
+ * @param rightX: x-coordinate of the center of the right pane in dual portrait mode
+ * @param middleX: x-coordinate of the center of the device in dual portrait mode
+ * @param middleY: y-coordinate of the center of the device in dual portrait mode
+ * @param bottomY: y-coordinate of the bottom of the device in dual portrait mode
+ * @param spanSteps: number of move steps to take when executing a span gesture, where one step takes ~ 5ms
+ * @param unspanSteps: number of move steps to take when executing a unspan gesture, where one step takes ~ 5ms
+ * @param switchSteps: number of move steps to take when executing a switch gesture, where one step takes ~ 5ms
+ * @param closeSteps: number of move steps to take when executing a close gesture, where one step takes ~ 5ms
  */
 enum class DeviceModel(
     val paneWidth: Int,
     val paneHeight: Int,
-    val foldSize: Int,
+    val foldWidth: Int,
     val leftX: Int = paneWidth / 2,
-    val rightX: Int = leftX + paneWidth + foldSize,
-    val middleX: Int = paneWidth + foldSize / 2,
+    val rightX: Int = leftX + paneWidth + foldWidth,
+    val middleX: Int = paneWidth + foldWidth / 2,
     val middleY: Int = paneHeight / 2,
     val bottomY: Int,
     val spanSteps: Int = 400,
@@ -35,9 +51,9 @@ enum class DeviceModel(
     val switchSteps: Int = 100,
     val closeSteps: Int = 50,
 ) {
-    SurfaceDuo(paneWidth = 1350, paneHeight = 1800, foldSize = 84, bottomY = 1780),
-    SurfaceDuo2(paneWidth = 1344, paneHeight = 1892, foldSize = 66, bottomY = 1870),
-    Other(paneWidth = 0, paneHeight = 0, foldSize = 0, bottomY = 0);
+    SurfaceDuo(paneWidth = 1350, paneHeight = 1800, foldWidth = 84, bottomY = 1780),
+    SurfaceDuo2(paneWidth = 1344, paneHeight = 1892, foldWidth = 66, bottomY = 1870),
+    Other(paneWidth = 0, paneHeight = 0, foldWidth = 0, bottomY = 0);
 
     override fun toString(): String {
         return "$name [leftX: $leftX rightX: $rightX middleX: $middleX middleY: $middleY bottomY: $bottomY]"
@@ -53,14 +69,14 @@ fun UiDevice.isSurfaceDuo(): Boolean {
 }
 
 /**
- * Returns the hinge/fold size of a device
+ * Returns the hinge/fold size of a device in pixels
  */
 fun UiDevice.getFoldSize(): Int {
-    return getDeviceModel().foldSize
+    return getDeviceModel().foldWidth
 }
 
 /**
- * Determines the model of a device based on display width and height
+ * Determines the model of a device based on display width and height (in pixels)
  */
 fun UiDevice.getDeviceModel(): DeviceModel {
     Log.d(
@@ -78,12 +94,14 @@ fun UiDevice.getDeviceModel(): DeviceModel {
 /**
  * Helper method to compare the pane width of a device to the pane widths of the defined device
  * models
+ *
+ * @param paneWidth: pane width in pixels
  */
 private fun UiDevice.getModelFromPaneWidth(paneWidth: Int): DeviceModel {
     for (model in DeviceModel.values()) {
         // pane width could be the width of a single pane, or the width of two panes + the width
         // of the hinge
-        if (paneWidth == model.paneWidth || paneWidth == model.paneWidth * 2 + model.foldSize)
+        if (paneWidth == model.paneWidth || paneWidth == model.paneWidth * 2 + model.foldWidth)
             return model
     }
     Log.d(
--- ComposeTesting/library/src/main/java/com/microsoft/device/dualscreen/composetesting/ZoomCoordinates.kt
+++ ComposeTesting/library/src/main/java/com/microsoft/device/dualscreen/composetesting/ZoomCoordinates.kt
@@ -7,6 +7,14 @@ package com.microsoft.device.dualscreen.composetesting
 
 import androidx.compose.ui.geometry.Offset
 
+/**
+ * Stores pinch start and end coordinates in order to simulate a zooming gesture
+ *
+ * @param leftOuter: outer value (closer to edge of screen/zero) for the left finger
+ * @param leftInner: inner value (closer to center of screen/max width) for the left finger
+ * @param rightOuter: outer value (closer to edge of screen/max width) for the right finger
+ * @param rightInner: inner value (closer to center of screen/zero) for the right finger
+ */
 data class ZoomCoordinates(
     val leftOuter: Offset,
     val leftInner: Offset,
