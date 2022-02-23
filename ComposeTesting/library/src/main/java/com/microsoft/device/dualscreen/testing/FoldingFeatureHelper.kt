423326cbdb6b70b141cadc5908f0bb1f419f9292
Kristen Halper <khalp@vt.edu>
2022-02-23 10:31:30 -0500

    Update to use kotlin testing lib

--- ComposeTesting/library/src/main/java/com/microsoft/device/dualscreen/testing/FoldingFeatureHelper.kt
+++ ComposeTesting/library/src/main/java/com/microsoft/device/dualscreen/testing/FoldingFeatureHelper.kt
@@ -9,11 +9,7 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.window.layout.FoldingFeature
-import androidx.window.testing.layout.FoldingFeature
-import androidx.window.testing.layout.TestWindowLayoutInfo
-import androidx.window.testing.layout.WindowLayoutInfoPublisherRule
 import org.junit.rules.TestRule
-import java.lang.ClassCastException
 
 /**
  * FOLDINGFEATURE HELPER
@@ -21,15 +17,6 @@ import java.lang.ClassCastException
  * These functions can be used in foldable UI tests to simulate the present of vertical and
  * horizontal foldingFeatures(folds/hinges). The foldingFeatures are simulated using TestWindowLayoutInfo.
  */
-
-/**
- * Return WindowLayoutInfoPublisherRule which allows you to push through different WindowLayoutInfo
- * values on demand from Window.testing to test
- *
- */
-fun createWindowLayoutInfoPublisherRule(): TestRule {
-    return WindowLayoutInfoPublisherRule()
-}
 
 /**
  * Simulate a vertical foldingFeature in a Compose test
@@ -63,89 +50,4 @@ fun <A : ComponentActivity> TestRule.simulateHorizontalFoldingFeature(
     state: FoldingFeature.State = FoldingFeature.State.HALF_OPENED
 ) {
     simulateHorizontalFoldingFeature(composeTestRule.activityRule, center, size, state)
-}
-
-/**
- * Simulate a foldingFeature with the given properties in a Compose test
- *
- * @param composeTestRule: Compose android test rule
- * @param center: location of center of foldingFeature
- * @param size: size of foldingFeature
- * @param state: state of foldingFeature
- * @param orientation: orientation of foldingFeature
- */
-fun <A : ComponentActivity> TestRule.simulateFoldingFeature(
-    composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<A>, A>,
-    center: Int,
-    size: Int,
-    state: FoldingFeature.State,
-    orientation: FoldingFeature.Orientation,
-) {
-    simulateFoldingFeature(composeTestRule.activityRule, center, size, state, orientation)
-}
-
-/**
- * Simulate a vertical foldingFeature
- *
- * @param activityRule: test activity rule
- * @param center: location of center of foldingFeature
- * @param size: size of foldingFeature
- * @param state: state of foldingFeature
- */
-private fun <A : ComponentActivity> TestRule.simulateVerticalFoldingFeature(
-    activityRule: ActivityScenarioRule<A>,
-    center: Int = -1,
-    size: Int = 0,
-    state: FoldingFeature.State = FoldingFeature.State.HALF_OPENED
-) {
-    simulateFoldingFeature(activityRule, center, size, state, FoldingFeature.Orientation.VERTICAL)
-}
-
-/**
- * Simulate a horizontal foldingFeature
- *
- * @param activityRule: test activity rule
- * @param center: location of center of foldingFeature
- * @param size: size of foldingFeature
- * @param state: state of foldingFeature
- */
-private fun <A : ComponentActivity> TestRule.simulateHorizontalFoldingFeature(
-    activityRule: ActivityScenarioRule<A>,
-    center: Int = -1,
-    size: Int = 0,
-    state: FoldingFeature.State = FoldingFeature.State.HALF_OPENED
-) {
-    simulateFoldingFeature(activityRule, center, size, state, FoldingFeature.Orientation.HORIZONTAL)
-}
-
-/**
- * Simulate a foldingFeature with the given properties
- *
- * @param activityRule: test activity rule
- * @param center: location of center of foldingFeature
- * @param size: size of foldingFeature
- * @param state: state of foldingFeature
- * @param orientation: orientation of foldingFeature
- */
-private fun <A : ComponentActivity> TestRule.simulateFoldingFeature(
-    activityRule: ActivityScenarioRule<A>,
-    center: Int,
-    size: Int,
-    state: FoldingFeature.State,
-    orientation: FoldingFeature.Orientation,
-) {
-    this as? WindowLayoutInfoPublisherRule
-        ?: throw ClassCastException("Test rule is not of type WindowLayoutInfoPublisherRule")
-
-    activityRule.scenario.onActivity { activity ->
-        val foldingFeature = FoldingFeature(
-            activity = activity,
-            center = center,
-            state = state,
-            size = size,
-            orientation = orientation
-        )
-        val windowLayoutInfo = TestWindowLayoutInfo(listOf(foldingFeature))
-        overrideWindowLayoutInfo(windowLayoutInfo)
-    }
 }
