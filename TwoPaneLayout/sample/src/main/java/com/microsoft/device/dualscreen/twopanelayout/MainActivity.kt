0b23c353ef613a98d7dd0733e34a5c3e602e5031
Kristen Halper <khalp@vt.edu>
2022-03-16 10:16:15 -0400

    Update sample to have padding around pane text

--- TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/MainActivity.kt
+++ TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -18,6 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.microsoft.device.dualscreen.twopanelayout.ui.theme.TwoPaneLayoutTheme
 
@@ -70,10 +72,11 @@ fun TwoPaneScope.Pane1() {
     Text(
         text = stringResource(R.string.first_pane_text),
         modifier = Modifier
-            .fillMaxSize()
-            .weight(.3f)
             .background(color = Color.Cyan)
-            .clickable { navigateToPane2() },
+            .clickable { navigateToPane2() }
+            .padding(10.dp)
+            .fillMaxSize()
+            .weight(.3f),
         color = Color.Black
     )
 }
@@ -83,10 +86,11 @@ fun TwoPaneScope.Pane2() {
     Text(
         text = stringResource(R.string.second_pane_text),
         modifier = Modifier
-            .fillMaxSize()
-            .weight(.7f)
             .background(color = Color.Magenta)
-            .clickable { navigateToPane1() },
+            .clickable { navigateToPane1() }
+            .padding(10.dp)
+            .fillMaxSize()
+            .weight(.7f),
         color = Color.Black
     )
 }
