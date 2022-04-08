afaaa22163ea34362aa95aad75d0b8e995bc44ee
Kristen Halper <khalp@vt.edu>
2022-04-08 10:18:58 -0400

    Update library sample to use TwoPaneLayout with navigation

--- TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/MainActivity.kt
+++ TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
@@ -14,6 +15,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -21,7 +23,13 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.microsoft.device.dualscreen.twopanelayout.ui.theme.TwoPaneLayoutTheme
+import com.microsoft.device.dualscreen.twopanelayout.ui.theme.blue
+import com.microsoft.device.dualscreen.twopanelayout.ui.theme.gray
+import com.microsoft.device.dualscreen.twopanelayout.ui.theme.red
+import com.microsoft.device.dualscreen.twopanelayout.ui.theme.yellow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,13 +47,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainPage() {
+    val navController = rememberNavController()
+
+    val pane1 = Pane("pane1") { Pane1(navController) }
+    val pane2 = Pane("pane2") { Pane2(navController) }
+    val pane3 = Pane("pane3") { Pane3(navController) }
+    val pane4 = Pane("pane4") { Pane4(navController) }
+
     Scaffold(
         topBar = { TopAppBar() },
         content = {
             TwoPaneLayout(
+                navController = navController,
                 paneMode = TwoPaneMode.HorizontalSingle,
-                pane1 = { Pane1() },
-                pane2 = { Pane2() }
+                panes = arrayOf(pane1, pane2, pane3, pane4),
+                singlePaneStartDestination = pane1.route,
+                pane1StartDestination = pane1.route,
+                pane2StartDestination = pane2.route
             )
         }
     )
@@ -68,29 +86,75 @@ fun TopAppBar() {
 }
 
 @Composable
-fun TwoPaneScope.Pane1() {
-    Text(
-        text = stringResource(R.string.first_pane_text),
-        modifier = Modifier
-            .background(color = Color.Cyan)
-            .clickable { navigateToPane2() }
-            .padding(10.dp)
-            .fillMaxSize()
-            .weight(.3f),
-        color = Color.Black
+fun TwoPaneScope.Pane1(navController: NavHostController) {
+    BasicPane(
+        text = R.string.first_pane_text,
+        color = blue,
+        textColor = Color.White,
+        navController = navController,
+        to = "pane2",
+        inPane = TwoPaneLayout.PaneContainer.PANE2
     )
 }
 
 @Composable
-fun TwoPaneScope.Pane2() {
-    Text(
-        text = stringResource(R.string.second_pane_text),
-        modifier = Modifier
-            .background(color = Color.Magenta)
-            .clickable { navigateToPane1() }
-            .padding(10.dp)
-            .fillMaxSize()
-            .weight(.7f),
-        color = Color.Black
+fun TwoPaneScope.Pane2(navController: NavHostController) {
+    BasicPane(
+        text = R.string.second_pane_text, color = gray, textColor = Color.White,
+        navController = navController,
+        to = "pane3",
+        inPane = TwoPaneLayout.PaneContainer.PANE2
     )
+}
+
+@Composable
+fun TwoPaneScope.Pane3(navController: NavHostController) {
+    BasicPane(
+        text = R.string.third_pane_text, color = yellow, textColor = Color.Black,
+        navController = navController,
+        to = "pane4",
+        inPane = TwoPaneLayout.PaneContainer.PANE1
+    )
+}
+
+@Composable
+fun TwoPaneScope.Pane4(navController: NavHostController) {
+    BasicPane(
+        text = R.string.fourth_pane_text, color = red, textColor = Color.White,
+        navController = navController,
+        to = "pane1",
+        inPane = TwoPaneLayout.PaneContainer.PANE1
+    )
+}
+
+@Composable
+private fun TwoPaneScope.BasicPane(
+    modifier: Modifier = Modifier,
+    text: Int,
+    color: Color,
+    textColor: Color,
+    navController: NavHostController,
+    to: String,
+    inPane: TwoPaneLayout.PaneContainer
+) {
+    Box {
+        Text(
+            text = stringResource(text),
+            modifier = modifier
+                .background(color = color)
+                .clickable { navController.navigateToPane(to, inPane) }
+                .padding(10.dp)
+                .fillMaxSize()
+                .align(Alignment.TopCenter),
+            color = textColor
+        )
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 20.dp),
+            text = "Navigates to $to, inside $inPane when in two pane mode",
+            color = textColor,
+            style = MaterialTheme.typography.h4
+        )
+    }
 }
--- TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/ui/theme/Color.kt
+++ TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val blue = Color(0xFF0086F0)
+val gray = Color(0xFF6E6E6E)
+val red = Color(0xFFE83A3A)
+val yellow = Color(0xFFFFC328)
