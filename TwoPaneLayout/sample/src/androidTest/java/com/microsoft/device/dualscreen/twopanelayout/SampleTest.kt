d8fd6a64a37bd4be43c02142f25771cee90a1ae2
Kristen Halper <khalper@microsoft.com>
2022-05-02 10:48:20 -0400

    Add back original TwoPaneLayout sample

---
+++ TwoPaneLayout/sample/src/androidTest/java/com/microsoft/device/dualscreen/twopanelayout/SampleTest.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+package com.microsoft.device.dualscreen.twopanelayout
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.microsoft.device.dualscreen.testing.compose.getString
+import com.microsoft.device.dualscreen.testing.compose.simulateHorizontalFoldingFeature
+import com.microsoft.device.dualscreen.testing.compose.simulateVerticalFoldingFeature
+import com.microsoft.device.dualscreen.testing.createWindowLayoutInfoPublisherRule
+import com.microsoft.device.dualscreen.twopanelayout.R
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+
+class SampleTest {
+    private val publisherRule = createWindowLayoutInfoPublisherRule()
+    private val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @get: Rule
+    val testRule: TestRule
+
+    init {
+        testRule = RuleChain.outerRule(publisherRule).around(composeTestRule)
+        RuleChain.outerRule(composeTestRule)
+    }
+
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {
+            MainPage()
+        }
+    }
+
+    @Test
+    fun app_launches() {
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.app_name)).assertIsDisplayed()
+    }
+
+    @Test
+    fun app_canNavigateToSecondPane() {
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.first_pane_text)).performClick()
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.second_pane_text)).assertIsDisplayed()
+    }
+
+    @Test
+    fun app_canNavigateToFirstPane() {
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.first_pane_text)).performClick()
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.second_pane_text)).performClick()
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.first_pane_text)).assertIsDisplayed()
+    }
+
+    @Test
+    fun app_dualPortrait_showsTwoPanes() {
+        publisherRule.simulateVerticalFoldingFeature(composeTestRule)
+
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.first_pane_text)).assertIsDisplayed()
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.second_pane_text)).assertIsDisplayed()
+    }
+
+    @Test
+    fun app_dualLandscape_showsOnePane() {
+        publisherRule.simulateHorizontalFoldingFeature(composeTestRule)
+
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.first_pane_text)).assertIsDisplayed()
+        composeTestRule.onNodeWithText(composeTestRule.getString(R.string.second_pane_text)).assertDoesNotExist()
+    }
+}
---
+++ TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.microsoft.device.dualscreen.twopanelayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.microsoft.device.dualscreen.twopanelayout.sample.ui.theme.TwoPaneLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TwoPaneLayoutTheme {
+                MainPage()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainPage() {
+    Scaffold(
+        topBar = { TopAppBar() },
+        content = {
+            TwoPaneLayout(
+                paneMode = TwoPaneMode.HorizontalSingle,
+                pane1 = { Pane1() },
+                pane2 = { Pane2() }
+            )
+        }
+    )
+}
+
+@Composable
+fun TopAppBar() {
+    TopAppBar(
+        title = {
+            BasicText(
+                text = stringResource(R.string.app_name),
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            )
+        }
+    )
+}
+
+@Composable
+fun BasicTwoPaneScope.Pane1() {
+    Text(
+        text = stringResource(R.string.first_pane_text),
+        modifier = Modifier
+            .background(color = Color.Cyan)
+            .clickable { navigateToPane2() }
+            .padding(10.dp)
+            .fillMaxSize()
+            .weight(.3f),
+        color = Color.Black
+    )
+}
+
+@Composable
+fun BasicTwoPaneScope.Pane2() {
+    Text(
+        text = stringResource(R.string.second_pane_text),
+        modifier = Modifier
+            .background(color = Color.Magenta)
+            .clickable { navigateToPane1() }
+            .padding(10.dp)
+            .fillMaxSize()
+            .weight(.7f),
+        color = Color.Black
+    )
+}
