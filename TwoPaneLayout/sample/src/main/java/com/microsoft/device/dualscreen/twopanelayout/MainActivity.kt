f29c6b7ecd87da3023108db6615636b5493117b6
Joy Liu <joyliu1@microsoft.com>
2022-05-05 13:45:07 -0700

    Update TwoPaneLayout sample colors (#32)
    * Update color palette
    
    * Move top bar inside each pane
    
    * Bump to version 1.0.0
    
    * Update screenshots for TwoPaneLayout sample
    
    * Update README
    
    * Adjust the screenshot size
    
    * Update screenshot
    
    Co-authored-by: Kristen Halper <khalper@microsoft.com>

--- TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/MainActivity.kt
+++ TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/MainActivity.kt
@@ -7,31 +7,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.microsoft.device.dualscreen.twopanelayout.ui.theme.TwoPaneLayoutTheme
+import com.microsoft.device.dualscreen.twopanelayout.ui.theme.blue
+import com.microsoft.device.dualscreen.twopanelayout.ui.theme.green
+import com.microsoft.device.dualscreen.twopanelayout.ui.theme.yellow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TwoPaneLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    MainPage()
-                }
+                MainPage()
             }
         }
     }
@@ -39,58 +33,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainPage() {
-    Scaffold(
-        topBar = { TopAppBar() },
-        content = {
-            TwoPaneLayout(
-                paneMode = TwoPaneMode.HorizontalSingle,
-                pane1 = { Pane1() },
-                pane2 = { Pane2() }
-            )
-        }
+    TwoPaneLayout(
+        paneMode = TwoPaneMode.HorizontalSingle,
+        pane1 = { Pane1() },
+        pane2 = { Pane2() }
     )
 }
 
 @Composable
 fun TopAppBar() {
     TopAppBar(
-        title = {
-            BasicText(
-                text = stringResource(R.string.app_name),
-                style = TextStyle(
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
-            )
-        }
+        title = { Text(text = stringResource(R.string.app_name), color = Color.White) },
+        backgroundColor = blue
     )
 }
 
 @Composable
 fun TwoPaneScope.Pane1() {
-    Text(
-        text = stringResource(R.string.first_pane_text),
-        modifier = Modifier
-            .background(color = Color.Cyan)
-            .clickable { navigateToPane2() }
-            .padding(10.dp)
-            .fillMaxSize()
-            .weight(.3f),
-        color = Color.Black
-    )
+    Scaffold(
+        topBar = { TopAppBar() }
+    ) {
+        Text(
+            text = stringResource(R.string.first_pane_text),
+            modifier = Modifier
+                .background(color = green)
+                .clickable { navigateToPane2() }
+                .padding(10.dp)
+                .fillMaxSize()
+                .weight(.3f),
+            color = Color.Black
+        )
+    }
 }
 
 @Composable
 fun TwoPaneScope.Pane2() {
-    Text(
-        text = stringResource(R.string.second_pane_text),
-        modifier = Modifier
-            .background(color = Color.Magenta)
-            .clickable { navigateToPane1() }
-            .padding(10.dp)
-            .fillMaxSize()
-            .weight(.7f),
-        color = Color.Black
-    )
+    Scaffold(
+        topBar = { TopAppBar() }
+    ) {
+        Text(
+            text = stringResource(R.string.second_pane_text),
+            modifier = Modifier
+                .background(color = yellow)
+                .clickable { navigateToPane1() }
+                .padding(10.dp)
+                .fillMaxSize()
+                .weight(.7f),
+            color = Color.Black
+        )
+    }
 }
--- TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/ui/theme/Color.kt
+++ TwoPaneLayout/sample/src/main/java/com/microsoft/device/dualscreen/twopanelayout/ui/theme/Color.kt
@@ -6,3 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val green = Color(0xFF88A43B)
+val yellow = Color(0xFFFFB652)
+val blue = Color(0xFF196DD7)
